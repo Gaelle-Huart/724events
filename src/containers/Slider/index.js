46baed86c68850e9bdd0d11c3800a4702655115b
Basmahx <138460878+Basmahx@users.noreply.github.com>
2025-01-31 04:18:53 +0100

    fixed the key error in the slider component

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,52 +7,55 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+
   const byDateDesc = data?.focus.sort(
-    (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date) // was changed to show the most recent event first.
+    (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date) // Sort by most recent event.
   );
 
-  // correction to prevent the white page from appearing
+  // Prevent the white page from appearing.
   const nextCard = () => {
     setTimeout(
       () => setIndex(index + 1 < byDateDesc.length ? index + 1 : 0),
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
   });
+
   return (
     <div className="SlideCardList">
+      {/* seperated the maps */}
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}>
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title} // event.id made the warning persist, was replaced with event.title
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}>
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx} // idx modified to index
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              key={`radio-${event.date}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
