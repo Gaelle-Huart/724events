4124d75a5163bc80de08a875345b36365ddecd93
Gaelle Huart <gaelle.c.huart@gmail.com>
2025-10-22 19:13:24 +0200

    Tests pour la navigation du site (Menu)
    index.test.js (src/containers/Menu/)
    - Ajout d'un test pour le lien "Nos services"
    - Ajout d'un test pour le lien "Nos réalisations"
    - Ajout d'un test pour le lien "Notre équipe"
    
    Tests tous passed

--- src/containers/Menu/index.test.js
+++ src/containers/Menu/index.test.js
@@ -11,7 +11,7 @@ describe("When Menu is created", () => {
   });
 
   describe("and a click is triggered on contact button", () => {
-    it("document location  href change", async () => {
+    it("document location href change", async () => {
       render(<Menu />);
       fireEvent(
         await screen.findByText("Contact"),
@@ -21,6 +21,60 @@ describe("When Menu is created", () => {
         })
       );
       expect(window.document.location.hash).toEqual("#contact");
+    });
+  });
+  
+  describe("and a click is triggered on services link", () => {
+    it("should change the current location to the selected href", async () => {
+      window.document.location.hash = "";
+      render(<Menu />);
+      const link = await screen.findByRole("link", { name: "Nos services" });
+      expect(link).toHaveAttribute("href", "#nos-services");
+      fireEvent(
+        link,
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      window.document.location.hash = link.getAttribute("href");
+      expect(window.document.location.hash).toEqual("#nos-services");
+    });
+  });
+
+  describe("and a click is triggered on realisations link", () => {
+    it("should change the current location to the selected href", async () => {
+      window.document.location.hash = "";
+      render(<Menu />);
+      const link = await screen.findByRole("link", { name: "Nos réalisations" });
+      expect(link).toHaveAttribute("href", "#nos-realisations");
+      fireEvent(
+        link,
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      window.document.location.hash = link.getAttribute("href");
+      expect(window.document.location.hash).toEqual("#nos-realisations");
+    });
+  });
+
+  describe("and a click is triggered on team link", () => {
+    it("should change the current location to the selected href", async () => {
+      window.document.location.hash = "";
+      render(<Menu />);
+      const link = await screen.findByRole("link", { name: "Notre équipe" });
+      expect(link).toHaveAttribute("href", "#notre-equipe");
+      fireEvent(
+        link,
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      window.document.location.hash = link.getAttribute("href");
+      expect(window.document.location.hash).toEqual("#notre-equipe");
     });
   });
 });
